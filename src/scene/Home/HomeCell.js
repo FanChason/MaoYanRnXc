48997f9a253da112111937d541b854cf24856475
FAN_XC <1091718619@qq.com>
2017-08-08 18:46:51 +0800

    调整电影列表UI布局

--- src/scene/Home/HomeCell.js
+++ src/scene/Home/HomeCell.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react'
-import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity, Image, TouchableWithoutFeedback } from 'react-native'
 import { Heading1, Heading2, Paragraph, Tip } from '../../widget/Text'
 import { screen } from '../../common'
 import { color } from '../../widget'
@@ -17,12 +17,28 @@ class HomeCell extends PureComponent {
                 {/*<Image source={require('../../img/zhanlang.png')} style={styles.icon} />*/}
 
                 <View style={styles.rightContainer}>
-                    <Heading1>{info.nm}</Heading1>
+                    <View style={styles.titleContainer}>
+                        <Heading1>{info.nm}</Heading1>
+                        <Text style={styles.movieStyle1}>{'3D'}</Text>
+                        <Text style={styles.movieStyle2}>IMAX</Text>
+                    </View>
 
                     <Tip style={styles.subtitle}>{ '观众评  '} <Text style={styles.score}>{info.sc}</Text></Tip>
                     <Tip style={styles.subtitle}>{ '主演：' + info.star}</Tip>
                     <Tip style={styles.subtitle}>{info.showInfo}</Tip>
                 </View>
+
+                <TouchableWithoutFeedback
+                    onPress={()=> {
+                        this.setState({count: this.state.count+1})
+                    }}
+                >
+                    <View style={styles.buybutton}>
+                        <Text style={styles.buttonText}>
+                            购买
+                        </Text>
+                    </View>
+                </TouchableWithoutFeedback>
 
             </TouchableOpacity>
         );
@@ -55,6 +71,41 @@ const styles = StyleSheet.create({
         paddingLeft: 20,
         paddingRight: 10,
     },
+
+    titleContainer :{
+        flex: 1,
+        flexDirection : 'row',
+        alignItems:'center',
+    },
+
+    movieStyle1:{
+        fontSize: 11,
+        color: 'white',
+        marginLeft: 15,
+        backgroundColor: 'gray',
+        borderRadius: 2,
+    },
+
+    movieStyle2:{
+        fontSize: 10,
+        color: 'gray',
+        marginLeft: 0.5,
+        borderColor: 'gray',
+        borderWidth: 1,
+        borderRadius: 2,
+    },
+    buybutton:{
+        borderRadius: 10,
+        alignItems:'center',
+    },
+
+    buttonText:{
+        color:'white',
+        width: 30,
+        height: 20,
+        backgroundColor:'red',
+    },
+
     price: {
         // color: color.theme
     }
